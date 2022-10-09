0e936b282dd89bad02f6d7bfae946b14e581be94
Gabriele Ciriano <cirianogabriele@gmail.com>
2022-10-09 13:51:04 -0300

    add: appsettings json, pasta service e classe adotante service

---
+++ ProjOng_Dapper/Config/DataBaseConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOng_Dapper.Config
+{
+    public class DataBaseConfiguration
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+        //responsável por captar a string de conexão
+        public static string Get()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsetting.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+            string _conn = Configuration["ConnectionStrings:DefaultConnection"];
+            return _conn; //retornando a string de conexão do appsettings.json
+
+        }
+    }
+}
---
+++ ProjOng_Dapper/Service/AdotanteService.cs
@@ -0,0 +1,15 @@
+﻿using ProjOng_Dapper.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjOng_Dapper.Service
+{
+    internal class AdotanteService
+    {
+        private IAdotanteRepository _adotanteRepository;
+
+    }
+}
